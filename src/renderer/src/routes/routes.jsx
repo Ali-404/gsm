e56062baad38b9268f61e5d614656794d5a6d34d
Ali-404 <abdellali.aitaddi@gmail.com>
2025-03-07 16:31:58 +0000

    Remove unused import of createBrowserRouter from react-router-dom

--- src/renderer/src/routes/routes.jsx
+++ src/renderer/src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createHashRouter } from 'react-router-dom'
+import {  createHashRouter } from 'react-router-dom'
 import App from '../App'
 import Dashboard from '../pages/dashboard/Dashboard'
 import Stock from '../pages/stock/stock'
